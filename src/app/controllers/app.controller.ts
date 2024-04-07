3ef1c4ae4c862b40afe8238ed8bb66499e9a71c6
Louis Crabtree <ljc48@students.uwf.edu>
2024-04-07 12:25:22 -0500

    class work

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -28,13 +28,16 @@ export class AppController {
 
     // PROTECT THE HOMEPAGE
 
-    this.router.use((req: any, res: Response, next) => {
+    const enforceLogin = ((req: any, res: Response, next: any) => {
       if (req.session.user) {
         next();
       } else {
         res.redirect("/login");
       }
     });
+
+    // Security middleware
+    this.router.use(enforceLogin);
 
     // Serve the home page
     this.router.get("/", (req: any, res: Response) => {
